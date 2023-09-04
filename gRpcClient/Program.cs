342254ea1dc988524b6f86c66fe5d08ac29e37eb
hopmxod007@mail.ru <hopmxod007@mail.ru>
2023-09-04 16:25:22 +0700

    Добавьте файлы проекта.

---
+++ gRpcClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Net.Client;
+using gRpcClient;
+
+namespace gRpcClient // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest() { Name = "World"});
+
+            Console.WriteLine($"Hello {reply}");
+            Console.ReadKey();
+        }
+    }
+}
